7eaf2bf1cc64101a7b38a21ec9f8f1414a5ec864
AndrewZamora <andrewmichaelzamora@gmail.com>
2020-04-16 22:58:07 +0900

    Added English Keys

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const fs = require('fs');
+const readFile = (...args) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(...args, (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(data);
+        });
+    });
+}
+
 const googleCreds = require('./google-creds.json')
 // Imports the Google Cloud client library
 const { Translate } = require('@google-cloud/translate').v2;
@@ -22,4 +34,30 @@ async function translateText(text, target) {
     });
 }
 
-translateText(text, target).catch(err => console.log(err));
+const combineKeysAndValues = (keys, values) => {
+    let object = {};
+    for (let i = 0; i < keys.length; i++) {
+        if (keys[i]) {
+
+            object[keys[i]] = values[i];
+        } else {
+            object["index"] = value[i];
+        }
+
+    }
+    return object;
+}
+
+
+
+(async () => {
+    const expensesInJapanese = await readFile('./test.csv', 'utf8').catch(err => console.log(err));
+    const rowTitlesInJapanese = expensesInJapanese.split('\n')[0];
+    const rowTilesInEnglish = ["Index", "Transaction Date", "Debit Account Title", "Debit Amount", "Debit Tax Classification", "Debit Item", "Debit Department", "Debit Note", "Credit Account Title", "Credit Amount", "Credit Tax Classification", "Credit Item", "Credit Department", "Credit Note", "Vendors", "Remark", "Adjustment Journal Entry", "Credit Vendors", "Debit Vendors", "Archival Number", "Date Written", "Debit Account Title Code", "Credit Account Title Code", "Update Date", "Approval Status","Approved By","Approved Date", "Created By"];
+    const valueSets = expensesInJapanese.split('\n').splice(1, expensesInJapanese.length - 1).map(expense => {
+        return expense.replace(/"/g, '').split(',');
+    });
+    const expensesToBeTranslated = valueSets.map(set => combineKeysAndValues(rowTilesInEnglish, set));
+    console.log(expensesToBeTranslated);
+
+})()
